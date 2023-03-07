94d7602f32dbf85f99960da888dbc18c0dd4dd91
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 10:50:40 -0600

    just did my first commit

---
+++ git-review.js
@@ -0,0 +1,8 @@
+console.log("Practice my git commands");
+// cmd git init to iniatialize repository
+
+//.git is where allyour repository stuff is
+//ls-a will list all hidden folders
+//git status will let you know you have initialized
+//adding and stage same thing//
+// git add: individual files; git add .(all contents of the folder); git add -A(all folders in any subfolders)
