7db63faf2eef856e356580a3422f0a095b4168f9
Gino Torres Jr <gindogg878@icloud.com>
2023-03-07 11:06:05 -0600

    more updates

--- git-review.js
+++ git-review.js
@@ -6,3 +6,11 @@ console.log("Practice my git commands");
 //git status will let you know you have initialized
 //adding and stage same thing//
 // git add: individual files; git add .(all contents of the folder); git add -A(all folders in any subfolders)
+//rm -rf .git to remove the hidden folder and remove a repository//
+//git commit -m 'message'
+// can use all commits by using git log
+//git branch -M main
+//git branch
+//git remote add origin https://github.com/gindogg878/git-practice.git
+//check remote -v
+// git push origin main
